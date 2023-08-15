166628681bdd4a2a71e359f758c9424a70689cd1
underforest <detroit@list.ru>
2023-08-15 14:28:26 +0500

    lesson 6 correct

--- src/brain-calc-cod.js
+++ src/brain-calc-cod.js
@@ -5,28 +5,29 @@ const getCalc = (userName) => {
   let n = 0;
   console.log('What is the result of the expression?');
   while (n < 3) {
-    let num1 = Math.round(Math.random() * 100);
-    let num2 = Math.round(Math.random() * 100);
+    const num1 = Math.round(Math.random() * 100);
+    const num2 = Math.round(Math.random() * 100);
     const operators = ['+', '-', '*'];
-    let operator = operators[Math.floor(Math.random() * operators.length)];
+    const operator = operators[Math.floor(Math.random() * operators.length)];
     result = `${num1} ${operator} ${num2}`;
     console.log(`Question: ${result}`);
     const userAnswer = readlineSync.question('Your answer:');
     const calc = () => {
       if (operator === '+') {
         return num1 + num2;
-      } else if (operator === '-') {
-        return num1 - num2;
-      } else {
-        return num1 * num2;
       }
+      if (operator === '-') {
+        return num1 - num2;
+      }
+      return num1 * num2;
     };
     if (Number(userAnswer) === calc()) {
       console.log('Correct!');
       n += 1;
     } else {
       console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${calc()}'`
+        // eslint-disable-next-line comma-dangle
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${calc()}'\nLet's try again, ${userName}`
       );
       return;
     }
